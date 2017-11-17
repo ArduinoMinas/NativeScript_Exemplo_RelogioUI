a8ed4665266a90b9af8e6ead82d9ea17247ae6ad
consultoria@carlosdelfino.eti.br <consultoria@carlosdelfino.eti.br>
2017-11-16 23:27:26 -0300

    Adicionado Hora formatada.
    Entre outras atualizações.

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,22 +1,31 @@
 import {Observable} from 'data/observable';
+import * as intlModule from "nativescript-intl";
 
 export class HelloWorldModel extends Observable {
 
     private _counter1: number;
     private _counter2: number;
     private _counter3: number;
+    private _time: string;
     private _id1: number;
     private _id2: number;
     private _id3: number;
     private _id4: number;
 
+    private _dateFormat: intlModule.DateTimeFormat;
+
     constructor() {
+
         super();
 
         // Initialize default values.
         this._counter1 = 0;
         this._counter2 = 0;
         this._counter3 = 0;
+
+        this._dateFormat = new intlModule.DateTimeFormat('en-US', {'hour': 'numeric', 'minute': 'numeric', 'second': ''});
+
+        this.updateTime();
     }
 
     updateCounter3() {
@@ -25,12 +34,19 @@ export class HelloWorldModel extends Observable {
     }
 
     updateTime(){
-        this.notifyPropertyChange("time",this.time);
+      this._time = this.doDateFormat(new Date());
+      this.notifyPropertyChange("time",this._time);
     }
 
-    get time(): Date{
-        return new Date();
+    doDateFormat(dt){
+      let fmtDt = this._dateFormat.format(dt);
+      return fmtDt;
     }
+
+    get time(){
+      return this._time;
+    }
+
     get counter1(): number{
         return this._counter1;
     }
@@ -41,7 +57,7 @@ export class HelloWorldModel extends Observable {
     
     set counter1(value: number){
         this._counter1 = value;
-        this.notifyPropertyChange("counter1",value);
+       this.notifyPropertyChange("counter1",this._counter1);
    }
     
     get counter2(): number{
@@ -50,7 +66,7 @@ export class HelloWorldModel extends Observable {
     
     set counter2(value: number){
         this._counter2 = value;
-        this.notifyPropertyChange("counter2",value);
+       this.notifyPropertyChange("counter2",this._counter2);
     }
     
     public onTap() {
@@ -65,7 +81,7 @@ export class HelloWorldModel extends Observable {
         if(this._id2 == null){
             this._id2 = setInterval(()=>{
                 this.counter2++
-            }, 3);
+            }, 2);
         }else{
             clearTimeout(this._id2);
             this._id2 = null;
@@ -77,12 +93,11 @@ export class HelloWorldModel extends Observable {
             this._id3 = null;
         }
          if(this._id4 == null){
-            this._id4 = setInterval(()=>this.updateTime(),1);
+            this._id4 = setInterval(()=>this.updateTime(),1000);
         }else{
             clearTimeout(this._id4);
             this._id4 = null;
         }
         console.log("onTap");
-
     }
 }
